95eafa015c1093f3d72a13d33e452b6422e62b0a
derekkipkemoi <derekkipkemoi@gmail.com>
2022-07-26 09:01:12 +0300

    Changes

--- src/views/app-views/apps/advaceRequests/AdvanceRequestsContent.js
+++ src/views/app-views/apps/advaceRequests/AdvanceRequestsContent.js
@@ -5,17 +5,19 @@ import TSCRequests from './TSCRequests';
 import TopUpsRequests from './TopUpsRequests';
 import StopOrdersRequests from './StopOrdersRequests';
 import RefundsRequests from './RefundsRequests';
+import MainMenu from './MainMenu';
 class AdvanceRequestsContent extends Component {
     state = {  } 
     render() { 
         const { match } = this.props
         return (
             <Switch>
+                <Route path={`${match.url}/advance-requests-menu`} component={MainMenu} />
 				<Route path={`${match.url}/normal-requests`} component={NormalRequests} />
                 <Route path={`${match.url}/tsc-requests`} component={TSCRequests} />
                 <Route path={`${match.url}/topups-requests`} component={TopUpsRequests} />
                 <Route path={`${match.url}/stop-orders-requests`} component={StopOrdersRequests} />
-                <Route path={`${match.url}/refunds`} component={RefundsRequests} />
+                <Route path={`${match.url}/refunds-requests`} component={RefundsRequests} />
 			</Switch>
         );
     }
---
+++ src/views/app-views/apps/advaceRequests/MenuData.js
@@ -0,0 +1,25 @@
+
+import {  ArrowUpOutlined, CheckOutlined, StopOutlined, ArrowDownOutlined} from "@ant-design/icons";
+    
+export const MenuData = [
+    {
+        title: 'Normal Requests',
+        path: "normal-requests",
+        icon: CheckOutlined,
+    },
+    {
+        title: 'Stop Orders',
+        path: "stop-orders-requests",
+        icon: StopOutlined,
+    },
+    {
+        title: 'Top Ups',
+        path: "topups-requests",
+        icon: ArrowUpOutlined,
+    },
+    {
+        title: 'Refunds',
+        path: "refunds-requests",
+        icon: ArrowDownOutlined,
+    }
+]
